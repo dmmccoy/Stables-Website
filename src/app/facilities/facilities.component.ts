5111f526b894483e9c17418f2cb36d3e1c086412
dmmccoy <dmmccoy654@gmail.com>
2018-08-29 10:23:44 -0400

    basic skeleton laid out

--- src/app/facilities/facilities.component.ts
+++ src/app/facilities/facilities.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FacilitiesComponent implements OnInit {
 
+  slideIndex = 1;
   constructor() { }
 
   ngOnInit() {
+      this.showSlides(this.slideIndex);
   }
 
+
+  plusSlides(n){
+    this.showSlides(this.slideIndex += n);
+  }
+
+  currentSlide(n) {
+    this.showSlides(this.slideIndex = n);
+  }
+
+  showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {this.slideIndex = 1}
+    if (n < 1) {this.slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].setAttribute("style", "display: none;");
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[this.slideIndex-1].setAttribute("style", "display: block;");
+    dots[this.slideIndex-1].className += " active";
+  }
 }
