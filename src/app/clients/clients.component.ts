7a51f28df5cac51a30d14ab72780bd9716323fcb
dmmccoy <dmmccoy654@gmail.com>
2018-09-06 16:21:13 -0400

    Working on Clients

--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -9,9 +9,11 @@ export class ClientsComponent implements OnInit {
 
   images: any[];
   images2: any[];
+  slideIndex = 1;
   constructor() { }
 
   ngOnInit() {
+
     this.images = [];
         this.images.push({source:'src/images/SaraFeather.jpg', alt:'Description for Image 1', title:'Title 1'});
         this.images.push({source:'src/images/SaraFeather2.jpg', alt:'Description for Image 2', title:'Title 2'});
@@ -20,6 +22,31 @@ export class ClientsComponent implements OnInit {
         this.images2.push({source:'src/images/SaraFeather.jpg', alt:'Description for Image 1', title:'Title 1'});
         this.images2.push({source:'src/images/SaraFeather2.jpg', alt:'Description for Image 2', title:'Title 2'});
         this.images2.push({source:'src/images/SaraFeather3.jpg', alt:'Description for Image 2', title:'Title 3'});
+    this.showSlides(this.slideIndex);
   }
+
+   plusSlides(n): void {
+  this.showSlides(this.slideIndex += n);
+}
+
+currentSlide(n): void {
+  this.showSlides(this.slideIndex = n);
+}
+
+showSlides(n): void {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {this.slideIndex = 1}
+  if (n < 1) {this.slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].setAttribute("style", "display: none;");
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[this.slideIndex-1].setAttribute("style", "display: block;");
+  dots[this.slideIndex-1].className += " active";
+}
 
 }
