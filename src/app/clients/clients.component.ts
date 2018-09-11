bf5d24fbd08aee28e8424f695ef4cc3882894739
dmmccoy <dmmccoy654@gmail.com>
2018-09-11 16:57:33 -0400

    Client basis complete

--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -15,38 +15,15 @@ export class ClientsComponent implements OnInit {
   ngOnInit() {
 
     this.images = [];
-        this.images.push({source:'src/images/SaraFeather.jpg', alt:'Description for Image 1', title:'Title 1'});
-        this.images.push({source:'src/images/SaraFeather2.jpg', alt:'Description for Image 2', title:'Title 2'});
-        this.images.push({source:'src/images/SaraFeather3.jpg', alt:'Description for Image 2', title:'Title 3'});
+        this.images.push({source:'src/images/SaraFeather2.jpg', alt:'Description for Image 1', title:'Title 1'});
+        this.images.push({source:'src/images/SaraFeather3.jpg', alt:'Description for Image 2', title:'Title 2'});
+        this.images.push({source:'src/images/Feather2.jpg', alt:'Description for Image 2', title:'Title 3'});
     this.images2 = [];
-        this.images2.push({source:'src/images/SaraFeather.jpg', alt:'Description for Image 1', title:'Title 1'});
-        this.images2.push({source:'src/images/SaraFeather2.jpg', alt:'Description for Image 2', title:'Title 2'});
-        this.images2.push({source:'src/images/SaraFeather3.jpg', alt:'Description for Image 2', title:'Title 3'});
-    this.showSlides(this.slideIndex);
+        this.images2.push({source:'src/images/BlackJackHeather.jpg', alt:'Description for Image 1', title:'Title 1'});
+        this.images2.push({source:'src/images/BlackJackHeather2.jpg', alt:'Description for Image 2', title:'Title 2'});
+        this.images2.push({source:'src/images/BlackJackHeather3.jpg', alt:'Description for Image 2', title:'Title 3'});
   }
 
-   plusSlides(n): void {
-  this.showSlides(this.slideIndex += n);
-}
 
-currentSlide(n): void {
-  this.showSlides(this.slideIndex = n);
-}
-
-showSlides(n): void {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {this.slideIndex = 1}
-  if (n < 1) {this.slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].setAttribute("style", "display: none;");
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[this.slideIndex-1].setAttribute("style", "display: block;");
-  dots[this.slideIndex-1].className += " active";
-}
 
 }
