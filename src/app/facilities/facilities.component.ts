9f76bb0dd59b27509f8f80f8782dddf221243e64
dmmccoy <dmmccoy654@gmail.com>
2018-09-20 19:06:51 -0400

    getting bootstrap to work, which will help look prettier

--- src/app/facilities/facilities.component.ts
+++ src/app/facilities/facilities.component.ts
@@ -7,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FacilitiesComponent implements OnInit {
 
+  images: any[];
   constructor() { }
 
   ngOnInit() {
-  }
 
+  }
 
 }
