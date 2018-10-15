35745525c9ddbd44a9a5b934580408aeb4eb8092
dmmccoy <dmmccoy654@gmail.com>
2018-10-15 17:42:53 -0400

    Gallery (#7)
    * bootstrap should fix problems
    
    * bootstrap works
    
    * fix client carousel, add modal to gallery
    
    * working on design florishes
    
    * Gallery function working, needs style improvements

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -7,10 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-  constructor() { }
+    slideIndex = 1;
 
-  ngOnInit() {
+  constructor() {
 
   }
 
+  ngOnInit() {
+
+
+
+  }
+
+
+
+  openModal() {
+    document.getElementById('myModal').style.display = "block";
+  }
+
+  closeModal() {
+    document.getElementById('myModal').style.display = "none";
+  }
+
+
+
+  plusSlides(n) {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  currentSlide(n) {
+    this.showSlides(this.slideIndex = n);
+  }
+
+showSlides(slideIndex);
+  showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {this.slideIndex = 1}
+    if (n < 1) {this.slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        (slides[i] as HTMLElement).style.display = "none";
+    }
+    (slides[this.slideIndex-1] as HTMLElement).style.display = "block";
+  }
 }
